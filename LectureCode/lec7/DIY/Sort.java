61c6b592b1742749338f7ca56c0d0e7b1a6fdf31
shuang.hux <shuang.hux@gmail.com>
2017-04-15 22:20:17 +0100

    Lecture 7 codes updated

--- LectureCode/lec7/DIY/Sort.java
+++ LectureCode/lec7/DIY/Sort.java
@@ -2,17 +2,47 @@ public class Sort {
 
 	/** Returns input strings in sorted order. 
 	  * Non-destructive. */
-	public static String[] Sort(String[] inputs) {
+	public static String[] Sort(String[] inputs, int k) {
+		if (k == inputs.length) {
+			return inputs;
+		}
 
+		int minIndex = indexOfSmallest(inputs, k);
+		swap(inputs, k, minIndex);
+		return Sort(inputs, k + 1);
+	}
+
+	// Get the smallest element's index starts from k
+	public static int indexOfSmallest(String[] inputs, int k) {
+		int minIndex = k;
+		int cmp;
+		for (int i = k; i < inputs.length; i++) {
+			if (inputs[i].compareTo(inputs[minIndex]) < 0) {
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
+
+	// Change the postion of index a and index b
+	public static void swap(String[] inputs, int a, int b) {
+		String temp = inputs[a];
+		inputs[a] = inputs[b];
+		inputs[b] = temp;
 	}
 
 	/** Prints out all strings in inputs. */
 	public static void print(String[] inputs) {
+		for (int i = 0; i < inputs.length; i++) {
+			System.out.print(inputs[i] + " ");
+		}
+		System.out.println();
 	}
 
 	/** Outputs the command line arguments 
 	  * in alphabetical order. */
 	public static void main(String[] args) {
-		
+		String[] sorted = Sort(args, 0);
+		print(sorted);
 	}
 } 
--- LectureCode/lec7/DIY/TestSort.java
+++ LectureCode/lec7/DIY/TestSort.java
@@ -1,6 +1,33 @@
 /** Tests the methods of the Sort class */
-public class TestSort {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestSort {	
+	/** Tests the Sort.sort method */
+	@Test
+	public void testSort() {
+		String[] inputs = {"milvia", "zero", "josh", "trump"};
+		String[] expected = {"josh", "milvia", "trump", "zero"};
+		String[] actual = Sort.sort(inputs);
+
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	public void testIndexOfSmallest() {
+		String[] inputs = {"milvia", "zero", "josh", "trump"};
+		int expected = 2;
+		int actual = Sort.indexOfSmallest(inputs, 2);
+
+		assertEquals(expected, actual);
+
+		expected = 3;
+		actual = Sort.indexOfSmallest(inputs, 3);
+		assertEquals(expected, actual);
+
+	}
 
 	public static void main(String[] args) {
+		jh61b.junit.TestRunner.runTests("all", TestSort.class);
 	}
 } 
